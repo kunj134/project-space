0cf1f04ae0a1e6dea2e0316e7aacd095ba0106ff
kunj134 <114722786+kunj134@users.noreply.github.com>
2022-12-15 18:30:12 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo apt update -y 
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo apt install php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+sudo apt update -y && sudo apt upgrade -y
